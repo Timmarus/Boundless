cbdcb7ece76ad872fc9c6c048f7a86e77c50267f
Kevin Gao <keving.gao@mail.utoronto.ca>
2019-02-25 11:55:29 -0500

    Basic UI

---
+++ boundless/src/components/SettingsPage/Settings.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import { Redirect } from "react-router-dom";
+import SettingsForm from "./SettingsForm";
+
+export class Settings extends Component {
+  render() {
+    return (
+      <div className="row">
+        <div className="col-md-4 col-md-offset-4">
+          <SettingsForm />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Settings;
