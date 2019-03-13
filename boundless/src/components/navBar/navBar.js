91d4f8a6172ae238e8f12894145ba7c22b51477f
Abdulwasay <abdulwasay_mehar@live.com>
2019-03-13 19:47:19 -0400

    cleaned up imports

--- boundless/src/components/navBar/navBar.js
+++ boundless/src/components/navBar/navBar.js
@@ -1,5 +1,5 @@
-import React, { Component } from "react";
-import { Link, NavLink } from "react-router-dom";
+import React from "react";
+import { Link } from "react-router-dom";
 import { connect } from "react-redux";
 import * as actions from "../../actions/loginActions";
 import SignedInLinks from './signedInLinks';
