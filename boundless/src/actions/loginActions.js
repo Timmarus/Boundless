4eb9f18b1984498fb3e3ad8df47b13e3662e99a5
Abdulwasay <abdulwasay_mehar@live.com>
2019-03-25 14:42:26 -0400

    fixed err

--- boundless/src/actions/loginActions.js
+++ boundless/src/actions/loginActions.js
@@ -44,6 +44,7 @@ export function signUpUser(userInfo) {
                 year: userInfo.year,
                 program: userInfo.program,
                 courses: userInfo.courses,
+                lastSeen: new Date.now()
 
             })
         }).then(() => {
