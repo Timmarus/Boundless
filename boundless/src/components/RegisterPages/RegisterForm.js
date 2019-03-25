620fe06d243c53fac7275a3c7e8df0c265897981
William <Gan>
2019-03-25 19:26:00 -0400

    Added in transition

--- boundless/src/components/RegisterPages/RegisterForm.js
+++ boundless/src/components/RegisterPages/RegisterForm.js
@@ -1,11 +1,10 @@
 import React, { Component } from "react";
-import { Redirect } from "react-router-dom";
 import { connect } from "react-redux";
 import * as actions from "../../actions/loginActions";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { Link } from "react-router-dom";
+import { Redirect, Link, BrowserRouter, Switch, Route } from "react-router-dom";
 import { Button } from "reactstrap";
-
+import Home from "../homescreenPages/HomeScreen";
 import Select from "react-select";
 
 const courses = [
@@ -85,10 +84,19 @@ class RegisterForm extends Component {
       if (!this.state.email.includes("@")) {
         alert("Incorrect Email.");
       } else {
-        alert("Success");
         this.props.signUpUser(this.state);
       }
     }
+  }
+
+  render() {
+    return (
+      <BrowserRouter>
+          <Switch>
+            <Route path="/home" component={Home} />
+          </Switch>
+      </BrowserRouter>
+    );
   }
 
   handleAddCourse = option => {
